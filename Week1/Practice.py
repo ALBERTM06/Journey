c5e269785d3f2a949ec85ca1824e8a4287353c34
ALBERTM06 <mirichoalbert@gmail.com>
2025-09-02 23:40:54 +0300

    More practice

--- Week1/Practice.py
+++ Week1/Practice.py
@@ -3,9 +3,37 @@
 # Then print them out in a single sentence using string concatenation and f-strings
 
 #My Solution:
-Name = input("Enter your name: ")
-Age = int(input("Enter your age: "))
-Height = float(input("Enter your height in metres: "))
+# Name = input("Enter your name: ")
+# Age = int(input("Enter your age: "))
+# Height = float(input("Enter your height in metres: "))
+#
+# print(f"My name is {Name}, I am {Age} years old and {Height} metres tall!")
 
-print(f"My name is {Name}, I am {Age} years old and {Height} metres tall!")
+# 2. Operators
+# 👉 Write a program that:
+# Takes two numbers as input.
+# Prints their sum, difference, product, quotient, and modulus.
+# Uses comparison operators (>, <, ==) to compare them.
 
+#My Solution:
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+sum = x + y
+print(sum)
+
+difference = x - y
+print(difference)
+
+product = x * y
+print(product)
+
+mod = x % y
+print(mod)
+
+if x == y:
+    print("These numbers are equal")
+elif x > y:
+    print("The first number is greater than the second!")
+else:
+    print("The second number is greater than the first!")
