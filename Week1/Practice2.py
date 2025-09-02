dbd1dba228ae2df9f135c14dc91417e4b75a1038
ALBERTM06 <mirichoalbert@gmail.com>
2025-09-02 23:23:01 +0300

    Did some practice on week 1 concepts

---
+++ Week1/Practice2.py
@@ -0,0 +1,11 @@
+# 1. Variables & Data Types
+# 👉 Write a program that stores your: Name (string), Age (int) and Height (float)
+# Then print them out in a single sentence using string concatenation and f-strings
+
+#My Solution:
+Name = input("Enter your name: ")
+Age = int(input("Enter your age: "))
+Height = float(input("Enter your height in metres: "))
+
+print(f"My name is {Name}, I am {Age} years old and {Height} metres tall!")
+
