0216b99272cb8e5721f6a207446c3c30624697cd
ALBERTM06 <mirichoalbert@gmail.com>
2025-10-10 23:12:19 +0300

    Worked on error handling

--- Week 2/Error_Handling.py
+++ Week 2/Error_Handling.py
@@ -1,12 +1,25 @@
+# try:
+#     x = int(input("Enter a number: "))
+#     y = int(input("Enter another number: "))
+#     result = x / y
+# except ValueError:
+#     print("Please enter valid integers.")
+# except ZeroDivisionError:
+#     print("You can’t divide by zero.")
+# else:
+#     print("Result:", result)        # runs only if no exception
+# finally:
+#     print("Done.")# always runs
+
+#Example 2
 try:
     x = int(input("Enter a number: "))
-    y = int(input("Enter another number: "))
-    result = x / y
+    result = 10 / x
 except ValueError:
-    print("Please enter valid integers.")
+    print("Please enter a number, not text.")
 except ZeroDivisionError:
-    print("You can’t divide by zero.")
+    print("You can’t divide by zero!")
 else:
-    print("Result:", result)        # runs only if no exception
+    print(f"The result is {result}.")   # runs only if no error
 finally:
-    print("Done.")               # always runs
+    print("Program finished.")
