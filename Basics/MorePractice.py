e165e7818b9b102fe84f1cfb52858bba649c5d55
ALBERTM06 <mirichoalbert@gmail.com>
2026-03-08 02:38:42 +0300

    Deleted some files, adjusted learning, polished on the basics and picked up some numpy basics!!

---
+++ Basics/MorePractice.py
@@ -0,0 +1,51 @@
+my_list = [1,2,3]
+
+import numpy as np
+arr = np.array(my_list)
+#print(my_list)
+
+arr2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#print(arr2)
+
+#Built-in Numpy methods
+arr3 = np.arange(30).reshape(6,5)
+#print(arr3)
+
+#arr4 = np.zeros(3)
+arr4 = np.zeros((2,2))
+#print(arr4)
+
+#arr5 = np.ones(4)
+arr5 = np.ones((4,4))
+#print(arr5)
+
+arr6 = np.linspace(0,10,3)
+#print(arr6)
+
+arr7 = np.eye(3)
+#print(arr7)
+
+from numpy.random import random_integers
+from numpy.random import rand
+from numpy.random import randn
+
+arr8 = rand(2)
+#print(arr8)
+
+arr9 = randn(5,5)
+#print(arr9)
+
+arr10 = random_integers(0,50,25).reshape(5,5)
+print(arr10)
+print(arr10.shape)
+
+x  = arr10.max()
+y = arr10.min()
+
+ax = arr10.argmax()
+by = arr10.argmin()
+
+print(x)
+print(y)
+print(ax)
+print(by)
---
+++ Stuff/stuff.py
@@ -0,0 +1,4 @@
+import numpy as np
+arr_2d = np.arange(50).reshape(5,10)
+print(arr_2d)
+
