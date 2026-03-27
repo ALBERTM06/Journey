b24b9daf8bc4ad37df69b39c5e4a23b356fe21b1
ALBERTM06 <mirichoalbert@gmail.com>
2026-03-27 22:06:08 +0300

    Learned Input and output by reading different file  types in Pandas

---
+++ Pandas/InputOutput.py
@@ -0,0 +1,21 @@
+# import numpy as np
+# import pandas as pd
+#
+# #CSV Input
+# df = pd.read_csv('example.csv')
+# print(df)
+#
+# #Excel Input
+# pd.read_excel('Excel_Sample.xlsx',sheet_name = 'Sheet1')
+# df.to_excel('Excel_Sample2.xlsx', sheet_name = 'NewSheet')
+#
+# #HTML Input
+# df = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
+# df[0].head()
+#
+# #SQL
+# from sqlalchemy import create_engine
+# engine = create_engine('sqlite:///:memory:')
+# df.to_sql('data', engine)
+# sql_df = pd.read_sql('data',con=engine)
+# print(sql_df)
