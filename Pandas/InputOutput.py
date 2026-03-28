ca381df5c897962149de9131b710ca5306f277df
ALBERTM06 <mirichoalbert@gmail.com>
2026-03-28 21:52:21 +0300

    Learned how to read json files

--- Pandas/InputOutput.py
+++ Pandas/InputOutput.py
@@ -1,21 +1,27 @@
-# import numpy as np
-# import pandas as pd
-#
-# #CSV Input
+import numpy as np
+import pandas as pd
+
+#CSV Input
 # df = pd.read_csv('example.csv')
 # print(df)
-#
-# #Excel Input
+#df = pd.read_csv('sample.csv')
+#print(df.to_string())
+
+#Excel Input
 # pd.read_excel('Excel_Sample.xlsx',sheet_name = 'Sheet1')
 # df.to_excel('Excel_Sample2.xlsx', sheet_name = 'NewSheet')
-#
-# #HTML Input
+
+#HTML Input
 # df = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
 # df[0].head()
-#
-# #SQL
+
+#SQL
 # from sqlalchemy import create_engine
 # engine = create_engine('sqlite:///:memory:')
 # df.to_sql('data', engine)
 # sql_df = pd.read_sql('data',con=engine)
 # print(sql_df)
+
+#JSON files
+df = pd.read_json('json_sample.json')
+print(df.to_string())
